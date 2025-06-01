a7290c3b7557173c4279b7f5546a899fbac9b175
samtmorgan <sam.morgan@beacon-tech.co.uk>
2025-06-01 15:40:34 +0100

    Remove maximum-scale=1 from meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,10 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1 " />
 
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
