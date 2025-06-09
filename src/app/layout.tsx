548d4014fa392919798863390f22c05906701a6f
samtmorgan <sam.morgan@beacon-tech.co.uk>
2025-06-09 07:35:10 +0100

    Update metadata for improved SEO and clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,19 @@ export default function RootLayout({
           }
         `}
       </script>
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
